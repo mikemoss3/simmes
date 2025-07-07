3947c7353b2cbd0a644e4a95f711d670daa9da95
Michael Moss (NASA) <mjmoss@ndc.nasa.gov>
2025-07-07 17:36:26 -0400

    Updated find theta and pcode methods because I am no longer confined to a grid

--- simmes/util_packages/det_ang_dependence.py
+++ simmes/util_packages/det_ang_dependence.py
@@ -1,5 +1,5 @@
 import numpy as np
-# from astropy.io import fits
+from astropy.io import fits
 from pathlib import Path
 
 
@@ -7,7 +7,18 @@ path_here = Path(__file__).parent
 def find_grid_id(imx,imy):
 	"""
 	Method to find the Swift/BAT response matrix GridID based on the position of the source on the detector plane according to Lien et al 2012.
+
+	Attributes:
+	--------------
+	imx, imy : float, float 
+		The x and y position on the detector plane
+
+	Returns:
+	--------------
+	gridid : str
+		Grid ID code 
 	"""
+
 	imx_imy_info = np.genfromtxt(path_here.joinpath("files-det-ang-dependence/gridnum_imx_imy.txt"),dtype=[("GRIDID","U3"),("imx",float),("imxmin",float),("imxmax",float),("imy",float),("imymin",float),("imymax",float),("thetacenter",float),("pcode",float)])
 	# Based on imx and imy, determine which grid number to use
 	try:
@@ -20,29 +31,39 @@ def find_grid_id(imx,imy):
 def find_inc_ang(imx,imy):
 	"""
 	Method to calculate the incidence angle (in radians) from a given position on the detector plane 
+	
+	Attributes:
+	--------------
+	imx, imy : float, float 
+		The x and y position on the detector plane
+
+	Returns:
+	--------------
+	theta : float (in radians)
+		Incident angle with respect to the detector boresight
 	"""
 
-	""" # This is the correct way to find the incidence angle, but doesn't work with response function grid
 	theta = np.arctan( np.sqrt( imx**2 + imy**2 ) )
-	return theta
-	"""
-
-	imx_imy_info = np.genfromtxt(path_here.joinpath("files-det-ang-dependence/gridnum_imx_imy.txt"),dtype=[("GRIDID","U3"),("imx",float),("imxmin",float),("imxmax",float),("imy",float),("imymin",float),("imymax",float),("thetacenter",float),("pcode",float)])
-	# Based on imx and imy, determine which grid number to use
-	try:
-		theta = imx_imy_info['thetacenter'][(imx>=imx_imy_info['imxmin']) & (imx<=imx_imy_info['imxmax']) & (imy>=imx_imy_info['imymin']) & (imy<=imx_imy_info['imymax'])][0]
-		theta *= np.pi / 180
-	except:
-		theta = np.pi/2
 
 	return theta
 
 def find_pcode(imx,imy):
 	"""
 	Method to calculate the partial coding fraction on the detector plane for a given position on the detector plane 
+
+	Attributes:
+	--------------
+	imx, imy : float, float 
+		The x and y position on the detector plane
+
+	Returns:
+	--------------
+	theta : float (in radians)
+		Incident angle with respect to the detector boresight
+
 	"""
 
-	""" # This is the correct way to find the pcode, but doesn't work with response function grid	
+	# This is the correct way to find the pcode, but doesn't work with response function grid	
 	# Load pcode map image 
 	pcode_img = fits.getdata("./util_packages/files-det-ang-dependence/pcode-map.img",ext=0) # indexing as pcode_img[y-index, x-index]
 	# Load header from file
@@ -58,20 +79,20 @@ def find_pcode(imx,imy):
 	j = int( ( ( imy - pcode_img_header["CRVAL2T"]) / pcode_img_header["CDELT2T"]) + pcode_img_header["CRPIX2T"] )
 
 
-	# The given imx,imy may be calculated to be in the center of a pixel. To make this compatible with calling an index, we force it to be a integer.
+	# The given imx, imy may be calculated to be in the center of a pixel. 
+	# To make this compatible with calling an index, we force it to be a integer.
 	# This has the result of rounding down.
 
 	return pcode_img[j,i]
-	"""
 
-	imx_imy_info = np.genfromtxt(path_here.joinpath("files-det-ang-dependence/gridnum_imx_imy.txt"),dtype=[("GRIDID","U3"),("imx",float),("imxmin",float),("imxmax",float),("imy",float),("imymin",float),("imymax",float),("thetacenter",float),("pcode",float)])
-	# Based on imx and imy, determine which grid number to use
-	try:
-		pcode = imx_imy_info['pcode'][(imx>=imx_imy_info['imxmin']) & (imx<=imx_imy_info['imxmax']) & (imy>=imx_imy_info['imymin']) & (imy<=imx_imy_info['imymax'])][0]
-	except:
-		pcode = 0
+	# imx_imy_info = np.genfromtxt(path_here.joinpath("files-det-ang-dependence/gridnum_imx_imy.txt"),dtype=[("GRIDID","U3"),("imx",float),("imxmin",float),("imxmax",float),("imy",float),("imymin",float),("imymax",float),("thetacenter",float),("pcode",float)])
+	# # Based on imx and imy, determine which grid number to use
+	# try:
+	# 	pcode = imx_imy_info['pcode'][(imx>=imx_imy_info['imxmin']) & (imx<=imx_imy_info['imxmax']) & (imy>=imx_imy_info['imymin']) & (imy<=imx_imy_info['imymax'])][0]
+	# except:
+	# 	pcode = 0
 
-	return pcode
+	# return pcode
 
 
 def fraction_correction(imx, imy):
@@ -79,6 +100,16 @@ def fraction_correction(imx, imy):
 	Method that calculates and returns a correction fraction that was found to be needed for off-axis bursts. 
 	This factor is needed to correct for the FFT convolution that is used for Swift/BAT
 	This correction was empirically fit with a quadratic function, which is how the parameter values in this method were determined.
+
+	Attributes:
+	--------------
+	imx, imy : float, float 
+		The x and y position on the detector plane
+
+	Returns:
+	--------------
+	val : float
+		Correction fraction that was found to be needed for off-axis bursts
 	"""
 
 	pcode = find_pcode(imx, imy)
