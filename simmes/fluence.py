5f39285ecd089aac9176ce47d84bacb6683d2ecf
Michael Moss (NASA) <mjmoss@ndc.nasa.gov>
2025-10-09 12:07:28 -0400

    reverted fluence calc for different time bin sizes

--- simmes/fluence.py
+++ simmes/fluence.py
@@ -20,14 +20,16 @@ def calc_fluence(light_curve, duration, tstart):
 	subint_light_curve = light_curve[(light_curve['TIME'] >= tstart ) & (light_curve['TIME'] <= tstart+duration)]
 
 	# Calculate photon fluence of the light curve within the specified time interval
-	fluence = np.sum(subint_light_curve['RATE'])
+	fluence = np.sum(subint_light_curve['RATE']) * dt
 
 	# Calculate the average 1 second peak flux 
 	if dt < 1:
 		num_bins = int(np.ceil(1 / dt / 2)) # number of time bins that make up one second 
 
 		arg_max = np.argwhere(light_curve['RATE'] == np.max(light_curve['RATE']) )[0][0]
-		flux_peak_1s = np.mean( light_curve['RATE'][arg_max-num_bins: arg_max+num_bins] ) / dt
+		flux_peak_1s = np.mean( light_curve['RATE'][arg_max-num_bins: arg_max+num_bins] )
+	elif dt > 1:
+		flux_peak_1s = np.max(subint_light_curve['RATE']) / dt
 	else:
 		flux_peak_1s = np.max(subint_light_curve['RATE'])
 
