a662004e793695cfdf7ce733aef76c90e7e8abeb
Mike Moss <mikejmoss3@gmail.com>
2026-01-14 15:15:21 +0200

    Improved threshold redshift search

--- simmes/threshold_search.py
+++ simmes/threshold_search.py
@@ -29,20 +29,28 @@ class PARAMS:
 	# Set initial parameter values
 	A : float = 1.
 	K : float = 0.
+
 	B : float = 1.
 	C : float = 1.
 	Q : float = 1.
 	M : float = 1.
 	nu : float = 1.
 
-	# Set parameter bounds
+	# Parameter uncertainties
+	uncB : float = 0.
+	uncC : float = 0.
+	uncQ : float = 0.
+	uncM : float = 0
+	uncnu : float = 0.
+
+	# Parameter bounds
 	B_lo : float = 0.
 	B_hi : float = 10.
 
 	C_lo : float = 0.
 	C_hi : float = 2.
 
-	Q_lo : float = 1.
+	Q_lo : float = 0.
 	Q_hi : float = 20.
 
 	M_lo : float = -10.
@@ -67,17 +75,59 @@ class PARAMS:
 		------------------------
 		None
 		"""
-		self.B = B
-		self.C = C
-		self.Q = Q
-		self.M = M
-		self.nu = nu
+		if B is not None:
+			self.B = B
+		if C is not None:
+			self.C = C
+		if Q is not None:
+			self.Q = Q
+		if M is not None:
+			self.M = M
+		if nu is not None:
+			self.nu = nu
 
+	def __str__(self):
+		names = ["B", "C", "Q", "M", "nu"]
+		fields = [self.B, self.C, self.Q, self.M, self.nu]
 
-def find_detectoin_rate_curve(grb, detection_rate, trials, 
+		return '\n'.join("{} = {}".format(names[i], fields[i]) for i in range(len(names)))
+
+	def get(self, var):
+		"""
+		Method that returns the current value and uncertainty of the specified variable
+
+		Attributes:
+		------------------------
+		var = str
+			Variable field name
+
+		Returns:
+		------------------------
+		ret : np.array([float, float])
+			Array storing the current parameter value and uncertainty
+		"""
+		if var == "A":
+			ret = np.array([self.A, 0.])
+		if var == "K":
+			ret = np.array([self.K, 0.])
+		if var == "B":
+			ret = np.array([self.B, self.uncB])
+		if var == "C":
+			ret = np.array([self.C, self.uncC])
+		if var == "Q":
+			ret = np.array([self.Q, self.uncQ])
+		if var == "M":
+			ret = np.array([self.M, self.uncM])
+		if var == "nu":
+			ret = np.array([self.nu, self.uncnu])
+
+		return ret
+
+def sample_detectoin_rate_curve(grb, trials,
 	imx, imy, ndets, 
+	num_samples = 15,
 	bgd_size = 20, ndet_max=32768, band_rate_min=14, band_rate_max=350, 
-	multiproc=True, searches=1, workers = mp.cpu_count(),
+	multiproc=True, workers = mp.cpu_count(),
 	time_resolved=False, sim_triggers=False, verbose = False):
 	"""
 	Method used to estimate the highest redshift a given GRB could be observed
@@ -88,8 +138,6 @@ def find_detectoin_rate_curve(grb, detection_rate, trials,
 	------------------------
 	grb : GRB 
 		GRB class object that holds the template GRB
-	detection_rate : float
-		The ratio of successful detections to total trials desired by the user
 	trials : int 
 		Number of trials to perform at each sampled redshift 	
 	imx, imy : 	float, float 
@@ -117,66 +165,56 @@ def find_detectoin_rate_curve(grb, detection_rate, trials,
 
 	Returns:
 	------------------------
+	z_vals : np.ndarray([floats])
+		Array of sampled redshift values 
+	detection_rates : np.ndarray([floats])
+		Array of sampled detection rates at the given redshift values
 
 	"""
 
 	# Redshift values to evaluate detection rates at
-	z_vals = np.linspace(0.1, 15, num=15)
+	z_vals = np.linspace(grb.z, 15, num=num_samples)
 
 	# Array to store detections rates.
-	det_rates_arr = np.zeros(shape=len(z_vals))
+	detection_rates = np.zeros(shape=num_samples)
 
-	# if multiproc:
+	if multiproc:
 
-	# 	template_grbs = np.zeros(shape=searches, dtype=GRB)
-	# 	for i in range(searches):
-	# 		template_grbs[i] = grb.deepcopy() # Create template GRB copies
+		template_grbs = np.zeros(shape=num_samples, dtype=GRB)
+		for i in range(num_samples):
+			template_grbs[i] = grb.deepcopy() # Create template GRB copies
 
-	# 	# Set up partial function with positional arguments 
-	# 	parfunc = partial(_calc_det_rat, trials = trials,
-	# 							imx=imx, imy=imx, ndets=ndets,
-	# 							bgd_size = bgd_size, ndet_max=ndet_max, band_rate_min=band_rate_min, band_rate_max=band_rate_max, 
-	# 							time_resolved=time_resolved, sim_triggers=sim_triggers, verbose=False)
-
-	# 	# Set up a pool of workers
-	# 	pool = mp.Pool(processes=workers, initializer=_init_process_seed)
-	# 	# Run threshold searches
-	# 	results = pool.map(parfunc, template_grbs)
-
-	# 	# Place all result arrays into single structured array with format [("zth", float), ("ztrack", float)]
-	# 	results = np.hstack(results)
-
-	for i in range(len(z_vals)):
-		det_rates_arr[i] = _calc_det_rat(grb, z=z_vals[i], trials = trials,
+		# Set up partial function with positional arguments 
+		parfunc = partial(_calc_det_rat, trials = trials,
 								imx=imx, imy=imx, ndets=ndets,
 								bgd_size = bgd_size, ndet_max=ndet_max, band_rate_min=band_rate_min, band_rate_max=band_rate_max, 
-								time_resolved=time_resolved, sim_triggers=sim_triggers, verbose=verbose)
+								time_resolved=time_resolved, sim_triggers=sim_triggers, verbose=False)
 
-	params = PARAMS()
+		# Set up a pool of workers
+		pool = mp.Pool(processes=workers, initializer=_init_process_seed)
+		# Run threshold searches
+		results = pool.starmap(parfunc, list(zip(template_grbs, z_vals)))
 
-	# Fit detection rates with sigmoid to obtain empirical detection rates curve
-	popt, pcov = curve_fit(detection_rate_sigmoid_fitter, xdata=z_vals, ydata=det_rates_arr, 
-							p0=[params.B, params.C, params.Q, params.M, params.nu], 
-							bounds=[(params.B_lo, params.C_lo, params.Q_lo, params.M_lo, params.nu_lo), 
-									(params.B_hi, params.C_hi, params.Q_hi, params.M_hi, params.nu_hi)] )
+		# Place all result arrays into single structured array with format [("zth", float), ("ztrack", float)]
+		detection_rates = np.hstack(results)
+	else:
+		for i in range(len(z_vals)):
+			detection_rates[i] = _calc_det_rat(grb, z=z_vals[i], trials = trials,
+									imx=imx, imy=imx, ndets=ndets,
+									bgd_size = bgd_size, ndet_max=ndet_max, band_rate_min=band_rate_min, band_rate_max=band_rate_max, 
+									time_resolved=time_resolved, sim_triggers=sim_triggers, verbose=verbose)
 
-	# Store best-fit values in a parameter class
-	params.B = popt[0]
-	params.C = popt[1]
-	params.Q = popt[2]
-	params.M = popt[3]
-	params.nu = popt[4]
-
-	return params
+	return z_vals, detection_rates
 
 def _init_process_seed():
 	# Instance a random seed
 	seed()
 
+
 def _calc_det_rat(grb, z, trials,
 	imx, imy, ndets, 
 	bgd_size = 20, ndet_max=32768, band_rate_min=14, band_rate_max=350, 
-	time_resolved=False, sim_triggers=False):
+	time_resolved=False, sim_triggers=False, verbose=False):
 	"""
 	Calculates the ratio of successful detections vs the number of simulations performed, i.e., the detection ratio
 
@@ -216,12 +254,58 @@ def _calc_det_rat(grb, z, trials,
 	sim_results = many_simulations(grb, param_list, trials, resp_mat=resp_mat, 
 									ndet_max=ndet_max, band_rate_min=band_rate_min, band_rate_max=band_rate_max,
 									bgd_size=bgd_size,
-									time_resolved=time_resolved, sim_triggers=sim_triggers)  # Perform simulations of burst at this redshift
+									time_resolved=time_resolved, sim_triggers=sim_triggers, verbose=verbose)  # Perform simulations of burst at this redshift
 
 	detection_rate = len( sim_results[ sim_results['DURATION']>0 ] ) / trials  # Calculate ratio of successful detections
 
 	return detection_rate
 
+
+def fit_detection_rate_curve(z_vals, detection_rates, params=None, **kwargs):
+	"""
+	Method to fit the given sampled points from a detection rate curve using the scipy.curve_fit package
+
+	Attributes:
+	------------------------
+	z_vals : np.ndarray([floats])
+		Array of sampled redshift values 
+	detection_rates : np.ndarray([floats])
+		Array of sampled detection rates at the given redshift values
+	params : PARAMS, optional
+		Object to provide user-defined initial parameter values and parameter bounds
+
+	Returns:
+	------------------------
+	params : PARAMS
+		Object storing the best fit parameter values
+
+	"""
+
+	if params is None:
+		params = PARAMS()
+
+	# Fit detection rates with sigmoid to obtain empirical detection rates curve
+	popt, pcov = curve_fit(detection_rate_sigmoid_fitter, xdata=z_vals, ydata=detection_rates, 
+							p0=[params.B, params.C, params.Q, params.M, params.nu], 
+							bounds=[(params.B_lo, params.C_lo, params.Q_lo, params.M_lo, params.nu_lo), 
+									(params.B_hi, params.C_hi, params.Q_hi, params.M_hi, params.nu_hi)], **kwargs)
+
+	# Store best-fit values in a parameter class
+	params.B = popt[0]
+	params.C = popt[1]
+	params.Q = popt[2]
+	params.M = popt[3]
+	params.nu = popt[4]
+
+	perr = np.sqrt(np.diag(pcov))
+
+	params.uncB = perr[0]
+	params.uncC = perr[1]
+	params.uncQ = perr[2]
+	params.uncM = perr[3]
+	params.uncnu = perr[4]
+
+	return params
 
 def detection_rate_sigmoid_fitter(z, B, C, Q, M, nu):
 	"""
@@ -247,7 +331,7 @@ def detection_rate_sigmoid_fitter(z, B, C, Q, M, nu):
 
 	return val
 
-def inverse_sigmoid(detection_rate, B, C, Q, M, nu):
+def calc_z_threshold(detection_rate, B, C, Q, M, nu):
 	"""
 	Method to calculate the redshift at which a burst has the desired detection rate. 
 	This calculation uses the Inverse of the generalized logistics functions given the input parameter values.
