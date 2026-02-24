0efcb99f4e15eb9d22412d2fe056e296727eba82
Michael Moss (NASA) <mjmoss@ndc.nasa.gov>
2026-02-24 16:26:36 -0500

    Small bug fix

--- simmes/simulations.py
+++ simmes/simulations.py
@@ -301,7 +301,7 @@ def many_simulations(template_grb, param_list, trials,
 										ndet_max=ndet_max, band_rate_min=band_rate_min, band_rate_max=band_rate_max, 
 										time_resolved = time_resolved, sim_triggers=sim_triggers, sim_bgd=sim_bgd, sim_var=sim_var, bgd_size=bgd_size)
 
-			if trigger.flag is True:
+			if trigger_flag is True:
 				sim_results[["DURATION", "TSTART"]][sim_result_ind] = bayesian_t_blocks(synth_grb.light_curve, dur_per=dur_per) # Find the Duration and the fluence 
 
 				if sim_results['DURATION'][sim_result_ind] > 0:	
